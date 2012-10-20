7d96f0145d69718124d159ddd9efe302211a81fa
Dmitry <lynx.net@tut.by>
2012-10-20 15:08:51 +0300

    hotel service call with stubs

---
+++ HotelService/Entity/Hotel.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelService.Entity
+{
+    public class Hotel
+    {
+        public string Name { get; private set; }
+        public decimal Cost { get; private set; }
+        public string Currency { get; private set; }
+        public string ImageUrl { get; private set; }
+
+        public Hotel(string name, decimal cost, string currency, string imageUrl)
+        {
+            Name = name;
+            Cost = cost;
+            Currency = currency;
+            ImageUrl = ImageUrl;
+        }
+    }
+}
---
+++ HotelService/HotelSearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HotelService.EanService;
+
+namespace HotelService
+{
+    public class HotelSearch
+    {
+        public List<Hotel> GetAvaliableHotels(string city, int numberOfAdults, DateTime arrivalDate, DateTime departureDate)
+        {
+            var hotelList = GetHotelsData(city, numberOfAdults, arrivalDate, departureDate);
+            return null;
+        }
+
+        private HotelList GetHotelsData(string city, int numberOfAdults, DateTime arrivalDate, DateTime departureDate)
+        {
+            var hotelServicesClient = new HotelServicesClient();
+
+            var request = new HotelListRequest();
+            request.arrivalDate = arrivalDate.ToString("MM/dd/yyyy");
+            request.departureDate = departureDate.ToString("MM/dd/yyyy");
+            request.city = city;
+            request.cid = 55505;
+            request.apiKey = "qb8es7zetcad5s5atuadxt3f";
+            var room = new Room();
+            room.numberOfAdults = numberOfAdults;
+            request.RoomGroup = new[] { room };
+
+            hotelServicesClient.Open();
+
+            var hotelListResponse = hotelServicesClient.getList(request);
+            return hotelListResponse.HotelList;
+        }
+    }
+}
